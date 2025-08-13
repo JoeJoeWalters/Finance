197208d4281cc7b82722cbe2ec8e06eb73b2033f
Joe Walters <info@thenakeddeveloper.com>
2025-08-14 00:04:18 +0100

    Added blank test file for Iso 20022

---
+++ tests/Finance.Tests/PaymentTests/ISO20022Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.Tests.PaymentTests
+{
+    public class ISO20022Tests
+    {
+    }
+}
